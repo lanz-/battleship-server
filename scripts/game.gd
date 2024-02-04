71b89698289edc479a864d0334e987db25dd7ff5
Alexey Starkov <ipdmrd@mail.ru>
2024-02-04 22:27:41 +0300

    Working multiplayer

--- scripts/game.gd
+++ scripts/game.gd
@@ -10,6 +10,9 @@ var opponent_id: int = -1
 
 var state = WAITING_OPPONENT
 
+var host_ships = []
+var opponent_ships = []
+
 var _host_placed = false
 var _opponent_placed = false
 
@@ -24,16 +27,25 @@ func get_peer_role(id: int):
 	return UNKNOWN
 
 
+func get_peer_id(my_id: int):
+	if opponent_id == my_id:
+		return peer_id
+	
+	return opponent_id
+
+
 func opponent_found(id: int):
 	opponent_id = id
 	state = PLACING_SHIPS
 
 
-func placement_complete(id: int):
+func placement_complete(id: int, ship_list: Array):
 	if id == opponent_id:
 		_opponent_placed = true
+		opponent_ships = ship_list
 	elif id == peer_id:
 		_host_placed = true
+		host_ships = ship_list
 	
 	if _host_placed and _opponent_placed:
 		state = PLAYING
--- scripts/lobby.gd
+++ scripts/lobby.gd
@@ -24,15 +24,12 @@ func _on_peer_connected(id: int):
 func _on_peer_disconnected(id: int):
 	print("Disonnected ", id)
 	
-	var game = _find_game(id)
+	var game: Game = _find_game(id)
 	if game:
 		_games.erase(game)
+		var peer_id = game.get_peer_id(id)
+		peer_left_the_game.rpc_id(peer_id)
 		_update_game_list_on_peers()
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
 
 
 func _update_game_list_on_peers():
@@ -56,14 +53,14 @@ func _find_game(peer_id: int):
 
 
 @rpc("any_peer", "reliable")
-func set_game_list(game_list: Array):
+func set_game_list(_game_list: Array):
 	var sender_id = multiplayer.get_remote_sender_id()
 	
 	print("Game list set on %s" % sender_id)
 
 
 @rpc("any_peer", "reliable")
-func create_game(name: String):
+func create_game(game_name: String):
 	if len(_games) > 256:
 		print("Too many games already created")
 		return
@@ -76,43 +73,76 @@ func create_game(name: String):
 		return
 	
 	game = Game.new()
-	game.name = name
+	game.name = game_name
 	game.peer_id = sender_id
 	
 	_games.append(game)
-	print("%s created new game [%s]" % [sender_id, name])
+	print("%s created new game [%s]" % [sender_id, game_name])
 	
 	_update_game_list_on_peers()
-	create_game.rpc_id(sender_id, name)
+	create_game.rpc_id(sender_id, game_name)
 
 
 @rpc("any_peer", "reliable")
-func join_game(name: String):
+func join_game(game_name: String):
 	var sender_id = multiplayer.get_remote_sender_id()
 	
 	for game in _games:
-		if game.name != name:
+		if game.name != game_name:
 			continue
 		
 		if game.state == Game.WAITING_OPPONENT:
-			print("%s joined %s" % [sender_id, name])
+			print("%s joined %s" % [sender_id, game_name])
 			game.opponent_found(sender_id)
-			join_game.rpc_id(sender_id, name)
+			join_game.rpc_id(sender_id, game_name)
+			join_game.rpc_id(game.peer_id, game_name)
 			_update_game_list_on_peers()
 			return
 	
-	print("Couldn find game %s for peer %s" % [name, sender_id])
+	print("Couldn find game %s for peer %s" % [game_name, sender_id])
 
 
 @rpc("any_peer", "reliable")
-func placement_completed(_goes_first: bool):
+func placement_completed(ship_list):
 	var sender_id = multiplayer.get_remote_sender_id()
-	var game = _find_game(sender_id)
+	var game: Game = _find_game(sender_id)
 	if not game:
 		print("No game found for %s" % sender_id)
 		return
 	
-	game.placement_complete(sender_id)
-	var goes_first = (game.get_peer_role(sender_id) == Game.HOST)
-	print("Placement completed for %s, goes first %s" % [sender_id, goes_first])
-	placement_completed.rpc_id(sender_id, goes_first)
+	game.placement_complete(sender_id, ship_list)
+	
+	if game.state == Game.PLAYING:
+		print("Starting game %s between %s and %s" % [game.name, game.peer_id, game.opponent_id])
+		placement_completed.rpc_id(game.peer_id, game.opponent_ships)
+		placement_completed.rpc_id(game.opponent_id, game.host_ships)
+
+
+@rpc("any_peer", "reliable")
+func fire_at(pos: Vector2i):
+	var sender_id = multiplayer.get_remote_sender_id()
+	var game: Game = _find_game(sender_id)
+	if not game:
+		print("No game found for %s" % sender_id)
+		return
+	
+	var peer_id = game.get_peer_id(sender_id)
+	print("%s firing at %s" % [sender_id, pos])
+	fire_at.rpc_id(peer_id, pos)
+
+
+@rpc("any_peer", "reliable")
+func fence_fire_remote():
+	var sender_id = multiplayer.get_remote_sender_id()
+	var game: Game = _find_game(sender_id)
+	if not game:
+		print("No game found for %s" % sender_id)
+		return
+	
+	print("Fire fence for %s" % sender_id)
+	fence_fire_remote.rpc_id(game.get_peer_id(sender_id))
+
+
+@rpc("reliable")
+func peer_left_the_game():
+	pass
